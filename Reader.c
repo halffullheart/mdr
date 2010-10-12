fee81883bd0f1fda86bfb22928a8936fe1232852
Daniel Worthington <daniel.worthington@gmail.com>
2010-10-12 05:01:47 -0700

    How 'bout some abstraction?

--- Reader.c
+++ Reader.c
@@ -2,26 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
+char* addToString(char* existing, char* new, int* size, int growByIfNeeded);
+
 char* getHTML()
 {
-    const int memChunkSize = 1000;
+    const int memChunkSize = 200;
 
     char* body;
-    int usedLength = 0;
-    int allocations = 1;
+    int bodylen = memChunkSize;
     body = (char*)malloc(memChunkSize * sizeof(char));
 
     while(!feof(stdin)) {
-        char line[500];
-        fgets(line, 500, stdin);
-        usedLength += strlen(line) + 1; // Add 1 for null-terminator
-        if (usedLength > allocations * memChunkSize)
-        {
-            allocations++;
-            body = (char*)realloc(body, allocations * memChunkSize * sizeof(char));
-        }
-        strcat(body, line);
+        char line[memChunkSize];
+        fgets(line, memChunkSize, stdin);
+        body = addToString(body, line, &bodylen, memChunkSize);
     }
 
     return body;
+}
+
+char* addToString(char* existing, char* new, int* size, int growByIfNeeded)
+{
+    if (strlen(existing) + strlen(new) + 1 > *size) // Add 1 to account for null-terminator
+    {
+        existing = (char*)realloc(existing, (*size + growByIfNeeded) * sizeof(char));
+        *size += growByIfNeeded;
+    }
+    strcat(existing, new);
+    return existing;
 }
