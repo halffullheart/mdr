c7cd2279b984fcd59b5bfa2df08d1715238c4fb0
Daniel Worthington <daniel.worthington@gmail.com>
2010-10-12 05:43:39 -0700

    Some improvements. Not totally sure. I'm tired.

--- Reader.c
+++ Reader.c
@@ -2,31 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* addToString(char* existing, char* new, int* size, int growByIfNeeded);
+#define CHUNK_SIZE 500
+
+char* newString(int* size);
+char* addToString(char* existing, char* new, int* size);
 
 char* getHTML()
 {
-    const int memChunkSize = 200;
-
-    char* body;
-    int bodylen = memChunkSize;
-    body = (char*)malloc(memChunkSize * sizeof(char));
+    int bodyLen = 0;
+    char* body = newString(&bodyLen);
 
     while(!feof(stdin)) {
-        char line[memChunkSize];
-        fgets(line, memChunkSize, stdin);
-        body = addToString(body, line, &bodylen, memChunkSize);
+        char line[CHUNK_SIZE];
+        fgets(line, CHUNK_SIZE, stdin);
+        body = addToString(body, line, &bodyLen);
     }
 
     return body;
 }
 
-char* addToString(char* existing, char* new, int* size, int growByIfNeeded)
+char* newString(int* size)
+{
+    *size = CHUNK_SIZE;
+    return (char*)malloc(CHUNK_SIZE * sizeof(char));
+}
+
+char* addToString(char* existing, char* new, int* size)
 {
     if (strlen(existing) + strlen(new) + 1 > *size) // Add 1 to account for null-terminator
     {
-        existing = (char*)realloc(existing, (*size + growByIfNeeded) * sizeof(char));
-        *size += growByIfNeeded;
+        existing = (char*)realloc(existing, (*size + CHUNK_SIZE) * sizeof(char));
+        *size += CHUNK_SIZE;
     }
     strcat(existing, new);
     return existing;
