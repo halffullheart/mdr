88806b774ce00860d675d2e9ad64b489494a2de7
François Vaux <madx@yapok.org>
2013-11-04 13:04:29 +0100

    linux: Set default window size (same as OS X)

--- linux/MakeDiffReadable.c
+++ linux/MakeDiffReadable.c
@@ -42,7 +42,7 @@ int main (int argc, char * argv[])
         mainWin = gtk_window_new(GTK_WINDOW_TOPLEVEL);
         webView = WEBKIT_WEB_VIEW(webkit_web_view_new());
 
-
+        gtk_window_set_default_size(GTK_WINDOW(mainWin), 900, 600);
         gtk_window_set_title(GTK_WINDOW(mainWin), "mdr");
         g_signal_connect (mainWin, "destroy", G_CALLBACK(gtk_main_quit), NULL);
         gtk_container_add(GTK_CONTAINER(mainWin), GTK_WIDGET(webView));
