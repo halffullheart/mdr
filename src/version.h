ce0961745e3d91190749299691cde8677b22aae5
Daniel Worthington <daniel.worthington@gmail.com>
2014-10-31 14:04:31 -0700

    Version bump to v1.0.1

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
 #define MDR_APP_NAME            "MDR"
 #define MDR_APP_DESC            "Make Diff Readable"
-#define MDR_VERSION             1,0,0,0
-#define MDR_VERSION_STR         "v1.0.0"
+#define MDR_VERSION             1,0,1,0
+#define MDR_VERSION_STR         "v1.0.1"
